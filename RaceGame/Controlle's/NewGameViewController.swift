e49ca24953c4821e8defe6b88da60149c6f0ed03
Aleksandr_Hunko <Aliaksandr.Hunko@gmail.com>
2022-06-28 18:40:51 +0200

    RaceGame
    RG

---
+++ RaceGame/Controlle's/NewGameViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewGameViewController.swift
+//  RaceGame
+//
+//  Created by Aliaksandr Hunko on 28/06/2022.
+//
+
+import Foundation
+import UIKit
+class NewGameViewController: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .darkGray
+    
+    }
+}
---
+++ RaceGame/Controlle's/SettingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingViewController.swift
+//  RaceGame
+//
+//  Created by Aliaksandr Hunko on 28/06/2022.
+//
+
+import Foundation
+import UIKit
+class SettingViewController: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .darkGray
+    
+}
+}
