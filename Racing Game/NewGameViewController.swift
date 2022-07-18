d32aa2b2692cb899fd240ccd367abb1490781fbf
Aleksandr_Hunko <Aliaksandr.Hunko@gmail.com>
2022-07-18 16:30:54 +0200

    Update 18.07
    Update 18.07

--- Racing Game/NewGameViewController.swift
+++ Racing Game/NewGameViewController.swift
@@ -9,137 +9,170 @@ import UIKit
 
 class NewGameViewController: UIViewController {
     
-    let RoadImage = UIImage(named: "Road")
-    let CarImage = UIImage(named: "Car")
-    let StoneImage = UIImage(named: "Stone")
-    let RoadImageView = UIImageView()
-    let CarImageView = UIImageView()
-    var Stone1ImageView = UIImageView()
+    let BackgroundView = UIImageView()
+    let CarView = UIImageView()
+    let StoneView = UIImageView()
     
+    let NavigationBar = UIView()
     
+    let step: CGFloat = 50
     
-    let DownView = UIView()
-    let UpView = UIView()
+    private var variable = Variables()
     
-    let moveLeftBtn = UIButton()
-    let moveRightBtn = UIButton()
-    let moveLeftBtnIcon = UIImage(named: "LeftBtn")
-    let moveRightBtnIcon = UIImage(named: "RightBtn")
-    
-    
-    var xCar = 30
-    var yCar = 0
-     let widthCar = 120
-    let heightCar = 120
-    let step = 60
-    var isGameOver = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "New Game"
-        view.backgroundColor = .systemGray3
+        BackgroundView.bounds = view.bounds
+        BackgroundView.center = view.center
+        BackgroundView.image = UIImage(named: "Road")
+        view.addSubview(BackgroundView)
         
-        // Road View
+        NavigationBar.frame = CGRect(x: 0, y: 0, width: view.bounds.maxX, height: view.bounds.maxY / 10)
+        NavigationBar.backgroundColor = .systemGray3
+        view.addSubview(NavigationBar)
         
-        RoadImageView.frame = CGRect(x: 0 , y: view.bounds.minY + view.frame.height / 8, width: view.bounds.maxX, height: view.bounds.maxY)
-        RoadImageView.image = RoadImage
-        
-        // Car View
-        
-        CarImageView.frame = CGRect(x: xCar, y: Int(RoadImageView.bounds.maxY / 2) + heightCar + heightCar / 2 , width: widthCar, height: heightCar)
-        CarImageView.image = CarImage
-        
-        // Stone
-        
-        Stone1ImageView.frame = CGRect(x: RoadImageView.bounds.width / 4, y: RoadImageView.bounds.minY - 40, width: 80, height: 80)
-        Stone1ImageView.image = StoneImage
-        
-        
-        RoadImageView.addSubview(CarImageView)
-        RoadImageView.addSubview(Stone1ImageView)
-        
-        view.addSubview(RoadImageView)
-        
-        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(moveLeft))
-        swipeLeft.direction = .left
-        view.addGestureRecognizer(swipeLeft)
-        
-        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(moveRight))
-        swipeRight.direction = .right
-        view.addGestureRecognizer(swipeRight)
+        createCar()
+        swipeCar()
         
     }
-        
+    
     override func viewDidAppear(_ animated: Bool) {
-            super.viewDidAppear(animated)
-            
-            moveBarrier()
-            
-        }
-       
+        super.viewDidAppear(animated)
         
-    
-    
-    @objc func moveLeft() {
-        
-        if xCar > Int(RoadImageView.bounds.minX) + 10  {
-            
-            xCar -= step
-            
-        }
-        
-        CarImageView.frame = CGRect(x: xCar, y: Int(RoadImageView.bounds.maxY / 2) + heightCar + heightCar / 2 , width: widthCar, height: heightCar)
-        
-
+        moveStone()
         
     }
     
-    @objc func moveRight() {
+    func createCar() {
         
-        if xCar < Int(RoadImageView.bounds.maxX) - 100 {
-            
-            xCar += step
-            
-        }
+        variable.widthCar = BackgroundView.bounds.width / 5
+        variable.heightCar = variable.widthCar * 1.9
         
-        CarImageView.frame = CGRect(x: xCar, y: Int(RoadImageView.bounds.maxY / 2) + heightCar + heightCar / 2 , width: widthCar, height: heightCar)
+        variable.xCar = BackgroundView.bounds.maxX / 4 - variable.widthCar / 2
+        variable.yCar = BackgroundView.bounds.maxY - variable.heightCar - variable.heightCar / 4
+        
+        CarView.frame = CGRect(x: variable.xCar, y: variable.yCar, width: variable.widthCar, height: variable.heightCar)
+        CarView.image = UIImage(named: "Car")
+        view.addSubview(CarView)
         
     }
     
-    func moveBarrier() {
+    func createStone() {
         
+        variable.widthStone = BackgroundView.bounds.width / 6
+        variable.heightStone = BackgroundView.bounds.width / 8
         
-        Stone1ImageView.center.x = CGFloat(Int.random(in: 50 ..< Int(view.frame.width - 100)))
-        Stone1ImageView.center.y = 0
+        variable.xStone = CGFloat(Int.random(in: Int(BackgroundView.bounds.minX)...Int(BackgroundView.bounds.maxX) - Int(variable.widthStone * 1.5)))
+        variable.yStone = view.bounds.minY
         
-        UIView.animate(
-            withDuration: 3,
-            delay: 0,
-            options: .curveLinear
-        ) {
-            self.Stone1ImageView.frame.origin.y = self.CarImageView.frame.origin.y
-        } completion: { _ in
-            if self.CarImageView.frame.intersects(self.Stone1ImageView.frame) {
-                self.isGameOver = true
-                self.dismiss(animated: true)
-            }
+        StoneView.frame = CGRect(x: variable.xStone, y: variable.yStone, width: variable.widthCar, height: variable.heightStone)
+        StoneView.image = UIImage(named: "Stone")
+        BackgroundView.addSubview(StoneView)
+        
+    }
+    
+    func moveStone() {
+        
+        createStone()
+        
+        UIView.animate(withDuration: 1, delay: 0 , options: .curveLinear, animations: {
             
-            else
+            self.StoneView.frame.origin.y = self.CarView.frame.origin.y - self.StoneView.bounds.height + 1
             
-            {
-                UIView.animate(withDuration: 3, delay: 0, options: .curveLinear) {
-                    self.Stone1ImageView.frame.origin.y = self.view.frame.maxY + 100
+        }, completion: { _ in
+            
+            if self.StoneView.frame.intersects(self.CarView.frame) {
+                
+                self.alert()
+                
+            } else {
+                
+                UIView.animate(withDuration: 0.5, delay: 0, options: .curveLinear, animations: {
                     
-                }
+                    self.StoneView.frame.origin.y = self.BackgroundView.frame.maxY
+                    
+                }, completion: { _ in
+                    
+                    self.moveStone()
+                    
+                })
+            }
+        })
+    }
+    
+    func alert() {
+        
+        let alert = UIAlertController(title: "Game Over",message: "", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "New Game", style: .default) {
+            _ in
+            self.moveStone()
+            
+        })
+        
+        alert.addAction(UIAlertAction(title: "Quit", style: .default) {
+            _ in
+            self.navigationController?.popToRootViewController(animated: true)
+            
+        })
+        
+        self.present(alert, animated: true)
+        
+    }
+    
+    func swipeCar() {
+        
+        CarView.isUserInteractionEnabled = true
+        
+        let moveGesture = UIPanGestureRecognizer(target: self, action: #selector(moveCar(recognizer:)))
+        CarView.addGestureRecognizer(moveGesture)
+        
+    }
+    
+    @objc func moveCar(recognizer: UIPanGestureRecognizer){
+        
+        if recognizer.state == .changed {
+            
+            let translation: CGPoint = recognizer.translation(in: self.view)
+            CarView.center = CGPoint(x: CarView.center.x + translation.x, y: variable.yCar + variable.heightCar / 2)
+            recognizer.setTranslation(CGPoint.zero, in: self.view)
+            
+            if CarView.frame.minX < view.frame.minX {
                 
-            completion: { _ in
-                self.moveBarrier()
+                CarView.frame = CGRect(x: view.frame.minX, y: variable.yCar, width: variable.widthCar, height: variable.heightCar)
                 
             }
+            
+            if CarView.frame.maxX > view.frame.maxX {
+                
+                CarView.frame = CGRect(x: view.frame.maxX - variable.widthCar, y: variable.yCar, width: variable.widthCar , height: variable.heightCar)
+                
             }
         }
     }
 }
 
+extension NewGameViewController {
+    
+    // MARK: VARIABLES
+    
+    private struct Variables {
+        
+        var widthCar = CGFloat()
+        var heightCar = CGFloat()
+        
+        var xCar = CGFloat()
+        var yCar = CGFloat()
+        
+        var widthStone = CGFloat()
+        var heightStone = CGFloat()
+        
+        var xStone = CGFloat()
+        var yStone = CGFloat()
+        
+        var GameOver = false
+        
+    }
+}
 
